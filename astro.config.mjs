77f37f95b937e0a436930c2db046359c86357a80
Wazarr <jeremy.fraoua@gmail.com>
2024-08-25 14:10:15 +0200

    add blog layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
